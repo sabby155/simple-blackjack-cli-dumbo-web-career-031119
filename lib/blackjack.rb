53f4d676b6a6e830fbea34ed428c4f921df3f9c9
Sabrina Chowdhury <nebula155@gmail.com>
2019-01-30 18:51:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+  puts "# code #display_card_total here"
 end
 
 def prompt_user
